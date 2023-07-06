4c2e9851648e695b0b7da170736974c5830b1eec
unknown <abhijeet.butola@gmail.com>
2023-07-07 00:13:58 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -27,7 +29,7 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info('Train Test Split initiated')
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=32)
@@ -39,13 +41,16 @@ class DataIngestion:
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
             )
 
         except Exception as e:
             raise CustomException(e, sys)
         
 
-# if __name__=='__main__':
-#     obj=DataIngestion()
-#     obj.initiate_data_ingestion()
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
